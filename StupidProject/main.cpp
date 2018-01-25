1c039f2a11abc2df9cfa0d9e17c7808bb74201e1
shujaetdin <shujaetdin@gmail.com>
2018-01-25 08:17:38 -0700

    created new sample project

---
+++ StupidProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  StupidProject
+//
+//  Created by Din, Shujaet on 1/25/18.
+//  Copyright © 2018 Din, Shujaet. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
